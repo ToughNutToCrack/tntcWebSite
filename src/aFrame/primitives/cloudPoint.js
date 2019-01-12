ec2f2f778fa1cdd5e91fe748ac155e39cdded8f2
Erik Minarini <minarini.work@gmail.com>
2019-01-12 10:57:49 +0100

    cloudpoint presets support

--- src/aFrame/primitives/cloudPoint.js
+++ src/aFrame/primitives/cloudPoint.js
@@ -18,6 +18,8 @@ aframe.registerComponent('cloudpoint', {
         offsety: {type: 'number', default: 0},
         offsetz: {type: 'number', default: 0},
 
+        preset: {type: 'number', default: -1},
+
         size: {type: 'number', default: 0.3},
         point: {type: 'number', default: 7},
         color: { type: 'array', default: [ '#FF926B', '#424242' ]},
@@ -31,19 +33,45 @@ aframe.registerComponent('cloudpoint', {
         const c = this.data.maxz
         const size = this.data.size
 
-        const maxPoint = this.data.point
+        const usePreset = this.data.preset > -1 && presets.length-1 >= this.data.preset
+
+        const maxPoint = (usePreset)? presets[this.data.preset].positions.length : this.data.point
 
         let prevPos = '0 0 0'
-      
+
         for (let i = 0; i < maxPoint; i++) {
-            const x = this.data.offsetx + Math.random() * (a - (-a) + 1) -a 
-            const y = this.data.offsety + Math.random() * (b - (-b) + 1) -b 
-            const z = this.data.offsetz + Math.random() * (c - (-c) + 1) -c
-            
-            const r = Math.random() * (size + 0.1) 
+
+            const x = 0
+            const y = 0 
+            const z = 0
+
             const pos = x + " " + y + " " + z
+            const r = 0.1
+
+            if(usePreset){
+
+                pos = presets[this.data.preset].positions[i]
+                r = presets[this.data.preset].sizes[i]
+                console.log(pos)
+
+            }else{
+
+                x = this.data.offsetx + Math.random() * (a - (-a) + 1) -a 
+                y = this.data.offsety + Math.random() * (b - (-b) + 1) -b 
+                z = this.data.offsetz + Math.random() * (c - (-c) + 1) -c
+
+                pos = x + " " + y + " " + z
+
+                r = Math.random() * (size + 0.1) 
+
+            }
+            
+            
+            
+           
 
             const col = this.data.color[Math.floor(Math.random() * this.data.color.length)]
+
 
             const point = initElement('a-dodecahedron',{
                 color: col,
@@ -66,6 +94,7 @@ aframe.registerComponent('cloudpoint', {
             }
 
             if(this.data.movement){
+
                 point.setAttribute(
                     'animation__position',
                     {
@@ -81,11 +110,96 @@ aframe.registerComponent('cloudpoint', {
                 prevPos = pos
             }
     
-            this.el.appendChild(point)
+            this.el.appendChild(point)  
             
         }
 
+        console.log("-------------------")
+
     }
 })
+
+const presets = [
+    {
+        positions:[
+            '2.25 -3.75 0.03',
+            '-0.94 -1.96 0.27',
+            '0.064 -0.34 0.46',
+            '-1.22 -3.19 0.23',
+            '-1.26 0.26 0.96',
+            '0.05 -4.33 0.02',
+            '2.06 -3.022 0.55',
+            '2.33 1.94 0.18',
+            '0.41 -3.45 0.17',
+            '-0.44 -0.27 0.36',
+            '-1.21 -2.15 0.95',
+            '0.57 0.65 0.44',
+            '-1.45 0.23 0.83',
+            '0.02 -0.10 0.21',
+            '-0.543 -1.73 0.58',
+            '-1.04 1.85 0.98',
+            '-1.11 0.07 0.85',
+            '-0.97 -1.26 0.57',
+            '2.24 0.37 0.64',
+            '0.31 -1.59 0.60',
+            '-1.01 -2.99 0.74',
+            '-0.82 -0.72 0.61',
+            '-0.36 -0.14 0.64',
+            '1.55 -3.76 0.90',
+            '0.58 -1.39 0.03',
+            '1.88 -1.17 0.68',
+            '1.91 -3.97 0.96',
+            '1.06 -4.40 0.89',
+            '0.23 -3.50 0.77',
+            '-1.32 -3.49 0.44'
+        ],
+        sizes:[
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05',
+            '0.05'
+        ]
+    },
+    {
+        positions:[
+            '2.25 -3.75 0.03',
+            '-0.94 -1.96 0.27',
+            '0.064 -0.34 0.46'
+        ],
+        sizes:[
+            '0.1',
+            '0.1',
+            '0.1'
+        ]
+
+    }
+
+]
 
 export default cloudPoint
