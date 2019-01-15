40a8e1f82b0944a79d1239e31f44344a9f41997c
Erik Minarini <minarini.work@gmail.com>
2019-01-15 10:24:53 +0100

    parallax

--- src/aFrame/components/logo-handler.js
+++ src/aFrame/components/logo-handler.js
@@ -5,9 +5,6 @@ import { initElement } from '../lib/utils'
 const logoHandler = aframe.registerComponent('logo-handler', {
     init: function() {
 
-        this.currentX = ''
-        this.currentY = ''
-
         const quad = initElement('a-entity',{
             material: 'color: #000000; transparent: true; opacity: 0',
             geometry: 'primitive:plane; height: 2; width: 14'
@@ -39,62 +36,8 @@ const logoHandler = aframe.registerComponent('logo-handler', {
             }
         )
 
-        window.addEventListener('mousemove', (e) => {this.mousehandler(e.clientX, e.clientY)})
-
         this.el.appendChild(quad)
-    },
-
-    mousehandler(x, y){
-
-        if(this.currentX == '') 
-            this.currentX = x
-
-        if(this.currentY == '') 
-            this.currentY = y
-
-        let xdiff = x - this.currentX
-        let ydiff = y - this.currentY
-
-        this.currentX = x
-        this.currentY = y
-
-        const minx = -0.3
-        const maxx = 0.3
-
-        const miny = 2.5-0.3
-        const maxy = 2.5+0.3
- 
-        if(xdiff < 0){
-            this.el.object3D.position.x += Math.abs(xdiff/1000)
-        }else if(xdiff>0){
-            this.el.object3D.position.x -= Math.abs(xdiff/1000)
-        }
-
-        if(this.el.object3D.position.x > maxx){
-            this.el.object3D.position.x = maxx
-        }
-        if(this.el.object3D.position.x < minx){
-            this.el.object3D.position.x = minx
-        }
-
-
-        if(ydiff < 0){
-            this.el.object3D.position.y += Math.abs(ydiff/1000)
-        }else if(ydiff>0){
-            this.el.object3D.position.y -= Math.abs(ydiff/1000)
-        }
-
-        if(this.el.object3D.position.y > maxy){
-            this.el.object3D.position.y = maxy
-        }
-        if(this.el.object3D.position.y < miny){
-            this.el.object3D.position.y = miny
-        }
-
-
-        console.log(ydiff)
     }
-
 })
 
 export default logoHandler
--- src/components/AFrame.js
+++ src/components/AFrame.js
@@ -9,6 +9,7 @@ import scrollListener from '../aFrame/components/scroll-listener'
 import navigation from '../aFrame/systems/navigation'
 import logoHandler from '../aFrame/components/logo-handler'
 import cloudPoint from '../aFrame/primitives/cloudPoint'
+import parallax from '../aFrame/components/parallax'
 /* eslint-enable no-unused-vars */
 
 const styles = StyleSheet.create({
@@ -54,7 +55,7 @@ class AFrame extends Component {
                     </a-assets>
 
                     <a-entity id="level0">
-                        <a-entity obj-model="obj: #logo-obj;" position="0 2.5 -10" material="color: #424242" scale="1 1 1" logo-handler></a-entity> 
+                        <a-entity obj-model="obj: #logo-obj;" position="0 2.5 -10" material="color: #424242" scale="1 1 1" logo-handler ></a-entity> 
                         <sin-grid></sin-grid>
                     </a-entity>
 
@@ -67,9 +68,9 @@ class AFrame extends Component {
                     <a-entity id="level1" position="0 -10 -35" rotation="0 0 0">
                         <a-entity obj-model="obj: #towers-obj; mtl: #towers-mtl"  position="-3 -4 -10" rotation="0 20 0" scale="0.8 0.8 0.8"></a-entity>
                         <a-circle color="#CCC" position="0 -4 -10"  rotation="-90 0 0" radius="15"></a-circle>
-                        <a-entity obj-model="obj: #cloud-1-obj; mtl: #cloud-1-mtl" position="7 3 -12" rotation="0 0 0" scale="0.1 0.1 0.1"></a-entity>
-                        <a-entity obj-model="obj: #cloud-2-obj; mtl: #cloud-2-mtl" position="12 6 -15" rotation="0 0 0" scale="0.3 0.3 0.3"></a-entity>
-                        <a-entity obj-model="obj: #cloud-3-obj; mtl: #cloud-3-mtl" position="-13 5.5 -16" rotation="0 0 0" scale="0.5 0.3 0.1"></a-entity>
+                        <a-entity obj-model="obj: #cloud-1-obj; mtl: #cloud-1-mtl" position="7 3 -12" rotation="0 0 0" scale="0.1 0.1 0.1" parallax="rangex:0.2; rangey:0.2; "></a-entity>
+                        <a-entity obj-model="obj: #cloud-2-obj; mtl: #cloud-2-mtl" position="12 6 -15" rotation="0 0 0" scale="0.3 0.3 0.3" parallax="rangex:0.1; rangey:0.1; speed:1"></a-entity>
+                        <a-entity obj-model="obj: #cloud-3-obj; mtl: #cloud-3-mtl" position="-13 5.5 -16" rotation="0 0 0" scale="0.5 0.3 0.1" parallax="rangex:0.1; rangey:0.1; speed:0.5"></a-entity>
                         {/* <a-entity position="3 0 -9" 
                             particle-system=" 
                                 maxAge: 10;
@@ -92,8 +93,8 @@ class AFrame extends Component {
 
                     <a-entity id="level2" position="-0.65 -15 -70" rotation="0 0 0">
                         <a-plane id="divisor" position="0 1.5 -4" rotation="0 0 0" width="0.01" height="3" color="#9E9E9E"></a-plane>
-                        <a-image id="studioImg"src="#logoStudio" position="-3 2.5 -4" width="1" height="1.48"></a-image>
-                        <a-image id="broadcastImg" src="#logoBroadcast" position="3 2.5 -4" width="1" height="1.48"></a-image>
+                        <a-image id="studioImg"src="#logoStudio" position="-3 2.5 -4" width="1" height="1.48" parallax="rangex:0.05; rangey:0.05; speed:0.1"></a-image>
+                        <a-image id="broadcastImg" src="#logoBroadcast" position="3 2.5 -4" width="1" height="1.48" parallax="rangex:0.05; rangey:0.05; speed:0.1"></a-image>
                         <cloud-point position="-4 2.5 -6" cloudpoint="maxx:1.5; maxy:3; maxz:0; offsety: -1.5; point:30; size:0.07; movement: true; preset: 0 "></cloud-point>
                         <cloud-point position="4 2.5 -6" cloudpoint="maxx:1.5; maxy:3; maxz:0; offsety: -1.5; point:30; size:0.07; color:#818285, #000000, #ed1c24, #fcd703; movement: true; preset: 1"></cloud-point>
                     </a-entity>
