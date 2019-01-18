20b1938759fa279305bffca111abc56179e3dd46
Devid Farinelli <devid.farinelli@gmail.com>
2019-01-18 04:43:59 +0100

    lo tolgo

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
-import CacheRoute from 'react-router-cache-route'
 import AFrame from './AFrame'
 import AboutUs from './AboutUs'
 
