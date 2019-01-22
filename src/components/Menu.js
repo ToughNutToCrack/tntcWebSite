e4fdd196543e8c08da7180b9a39f9b9d4b922b48
Koaleo <ma7730@hotmail.it>
2019-01-22 22:05:27 +0100

    wip

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { StyleSheet, css } from 'aphrodite'
+import { NavLink } from 'react-router-dom'
+import { FaTwitter, FaFacebookF, FaYoutube, FaInstagram } from 'react-icons/fa'
+import { IconContext } from 'react-icons'
 
 const resizeKeyframes = {
     '0%': {
@@ -56,7 +59,47 @@ const styles = StyleSheet.create({
         height: '100%',
         bottom: '0px',
         color: 'white'
-    }
+    },
+
+    navigation: {
+        color: 'white',
+        position: 'relative',
+        margin: '0 auto',
+        opacity: 1
+    },
+    nav: {
+        display: 'inline-block',
+    },
+    navLink: {
+        fontFamily: '"Patua One", regular',
+        color: '#9E9E9E',
+        display: 'inline-block',
+        textAlign: 'center',
+        padding: '14px 16px',
+        textDecoration: 'none',
+        textTransform: 'uppercase',
+        fontWeight: 600,
+        fontSize: 22,
+    },
+    active: {
+        color: 'white',
+    },
+    socials: {
+        color: '#424242',
+        fontSize: 24,
+        width: '15%',
+        opacity: 1,
+        '@media only screen and (max-width: 880px)': {
+            position: 'absolute',
+            right: '0px',
+            minWidth: '140px'
+        }
+    },
+    socialIcon: {
+        color: '#424242',
+        width: '20%',
+        opacity: 1
+    },
 })
 
 class Menu extends Component {
@@ -80,6 +123,36 @@ class Menu extends Component {
             <div className={ this.state.isOpen ? css(styles.resizeAnim) : css(styles.closeMenu) }>              
                 <div className={ css(styles.title, styles.hover) } onClick={ this.handleMenu } >
                     Menu
+                </div>
+                <div className={ css(styles.navigation) }>
+
+                    <ul>
+                        <li className={ css(styles.nav) }>
+                            <NavLink className={ css(styles.navLink, styles.hover) } activeClassName={ css(styles.active) } exact to="/">Home</NavLink>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li className={ css(styles.nav) }>
+                            <NavLink className={ css(styles.navLink, styles.hover) } activeClassName={ css(styles.active) } to="/studio">Studio</NavLink>
+                        </li>
+                    </ul>
+
+                    <ul>
+                        <li className={ css(styles.nav) }>
+                            <NavLink className={ css(styles.navLink, styles.hover) } activeClassName={ css(styles.active) } to="/broadcast">Broadcast</NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className={ css(styles.nav) }>
+                            <NavLink className={ css(styles.navLink, styles.hover) } activeClassName={ css(styles.active) } to="/about-us">AboutUs</NavLink>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className={ css(styles.nav) }>
+                            <NavLink className={ css(styles.navLink, styles.hover) } activeClassName={ css(styles.active) } to="/contacts">Contacts</NavLink>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
